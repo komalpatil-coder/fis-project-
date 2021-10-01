3584c1af9d935b9d66bdeee0c16e645257e415a9
komalpatil-coder <91188600+komalpatil-coder@users.noreply.github.com>
2021-10-01 23:26:30 +0530

    Update Catalogue.java

--- src/Catalogue.java
+++ src/Catalogue.java
@@ -7,44 +7,26 @@ public class Catalogue implements Serializable{
     private List<Multimedia> mulList;
     private String catName;
 
-    /**
-     *
-     * @param _id
-     */
+   
     public Catalogue(String _id){
         this.catName = _id;
         this.mulList = new ArrayList<>();
     }
 
 
-    /**
-     *
-     * @return
-     */
     public String getCatName(){
         return this.catName;
     }
 
-    /**
-     *
-     * @return
-     */
     public List<Multimedia> getMultimediaList(){
         return this.mulList;
     }
 
-    /**
-     *
-     * @param _id
-     */
     public void setCatName(String _id){
         this.catName = _id;
     }
 
-    /**
-     *
-     * @param _list
-     */
+    
     public void setMultList(List<Multimedia> _list){
         this.mulList = _list;
     }
